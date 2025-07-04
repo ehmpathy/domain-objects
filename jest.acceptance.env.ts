f8dccbfb2a2ac2f331929820c0862416b3c2a072
Ulad Kasach <uladkasach@gmail.com>
2025-07-03 19:07:17 -0500

    fix(practs): bump to latest best

--- jest.acceptance.env.ts
+++ jest.acceptance.env.ts
@@ -1,2 +1,13 @@
 // eslint-disable-next-line no-undef
 jest.setTimeout(90000); // we're calling downstream apis
+
+/**
+ * .what = verify that the env has sufficient auth to run the tests; otherwise, fail fast
+ * .why =
+ *   - prevent time wasted waiting on tests to fail due to lack of credentials
+ *   - prevent time wasted debugging tests which are failing due to hard-to-read missed credential errors
+ */
+if (!(process.env.AWS_PROFILE || process.env.AWS_ACCESS_KEY_ID))
+  throw new Error(
+    'no aws credentials present. please authenticate with aws to run acceptance tests',
+  );
--- jest.integration.env.ts
+++ jest.integration.env.ts
@@ -13,3 +13,14 @@ if (
   process.env.I_KNOW_WHAT_IM_DOING !== 'true'
 )
   throw new Error(`integration.test is not targeting stage 'test'`);
+
+/**
+ * .what = verify that the env has sufficient auth to run the tests; otherwise, fail fast
+ * .why =
+ *   - prevent time wasted waiting on tests to fail due to lack of credentials
+ *   - prevent time wasted debugging tests which are failing due to hard-to-read missed credential errors
+ */
+if (!(process.env.AWS_PROFILE || process.env.AWS_ACCESS_KEY_ID))
+  throw new Error(
+    'no aws credentials present. please authenticate with aws to run integration tests',
+  );
