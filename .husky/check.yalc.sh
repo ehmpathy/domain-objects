f91899e49a7397a87df9fc9519a1ca862f2bacc6
Ulad Kasach <uladkasach@gmail.com>
2025-12-03 07:21:18 -0600

    fix(deps): bump uuid-fns depver

--- .husky/check.yalc.sh
+++ .husky/check.yalc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # https://github.com/wclr/yalc
-if [ "$(npx yalc check)" ]; then
-    echo "✋ package.json has yalc references. Run 'npx yalc remove --all' to remove these local testing references."
+if command -v npx &> /dev/null; then
+    if [ "$(npx yalc check 2>/dev/null)" ]; then
+        echo "✋ package.json has yalc references. Run 'npx yalc remove --all' to remove these local testing references."
+    fi
 fi
 
